9e3770809c404a01070f7d05bbe0404aa18ace44
Levente Gall <leventegall@proton.me>
2026-03-27 19:12:16 +0100

    WIP

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands
+from discord.ext import commands
 import os
 
 # This is a 'decorator' - it's a special function that checks something before running a command!
